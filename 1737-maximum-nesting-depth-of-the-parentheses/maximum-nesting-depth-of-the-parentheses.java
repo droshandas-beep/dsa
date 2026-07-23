3a9c5b6dde55104eaf27e4a07f0b76e107b8a236
Chidanand_05 <droshandas@gmail.com>
2026-07-24 00:31:38 +0530

    Time: 1 ms (23.75%) | Memory: 42.6 MB (94.41%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDepth(String s) {
+        Stack<Character> stack=new Stack<>();
+        int maxDepth=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                stack.push(ch);
+                maxDepth=Math.max(maxDepth,stack.size());
+            }
+            else if(ch==')'){
+                stack.pop();
+            }
+        }
+        return maxDepth;
+        
+    }
+}
