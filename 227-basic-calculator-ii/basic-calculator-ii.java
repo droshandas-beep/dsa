68113417822f138d1521743a62c1fa0810be50e6
Chidanand_05 <droshandas@gmail.com>
2026-07-25 03:51:34 +0530

    Time: 27 ms (18.22%) | Memory: 48.2 MB (56.51%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack=new Stack<>();
+        char sign='+';
+        int num=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(Character.isDigit(c)){
+                num=num*10+(c-'0');
+            }
+            if((!Character.isDigit(c) && c!=' ')|| i==s.length()-1){
+                if(sign=='+'){
+                    stack.push(num);
+                }
+                else if(sign=='-'){
+                    stack.push(-num);
+                }
+                else if(sign=='*'){
+                    stack.push(stack.pop()*num);
+                }
+                else if(sign=='/'){
+                    stack.push(stack.pop()/num);
+                }
+                sign=c;
+                num=0;
+            }
+        }
+        int result=0;
+        while(!stack.isEmpty()){
+            result+=stack.pop();
+        }
+        return result;
+    }
+}
